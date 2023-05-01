24d5d3e9557dd524625e917264ae8a6e81627dc2
Martin Bernstorff <martinbernstorff@gmail.com>
2023-05-01 14:00:41 +0200

    build: allow amending when committing from invoke

--- tasks.py
+++ tasks.py
@@ -121,10 +121,13 @@ def _add_commit(c: Context, msg: Optional[str] = None):
     c.run("git add .")
 
     if msg is None:
-        msg = input("Commit message: ")
+        msg = input("Commit message [any | --amend]: ")
 
-    c.run(f'git commit -m "{msg}"', pty=NOT_WINDOWS, hide=True)
-    print(f"{msg_type.GOOD} Changes added and committed")
+        if "--a" in msg:
+            c.run("git commit --amend --reuse-message=HEAD", pty=NOT_WINDOWS, hide=True)
+        else:
+            c.run(f'git commit -m "{msg}"', pty=NOT_WINDOWS, hide=True)
+    print(f"{msg_type.GOOD} Changes committed")
 
 
 def is_uncommitted_changes(c: Context) -> bool:
